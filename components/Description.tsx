444b578931f5c680ade19ca58356e40054173d59
AryaK-31 <aryakaslikar31@gmail.com>
2023-03-15 16:02:16 +0530

    try

--- components/Description.tsx
+++ components/Description.tsx
@@ -48,7 +48,7 @@ export default function Description() {
                             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
                             <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
                                 <h1 className="font-dmserif text-3xl font-bold text-white">Prize Money</h1>
-                                <p className="mb-10 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
+                                <p className="mb-10 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Cash prize of ₹7,500 and goodies.</p>
                                 <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See More</button>
                             </div>
                         </div>
