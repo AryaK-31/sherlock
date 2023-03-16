3620eb9b010db97d1887bad978d14ae8c4faab97
AryaK-31 <aryakaslikar31@gmail.com>
2023-03-16 12:00:15 +0530

    form changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  "compilerOptions":{
+    "strict":false,
+    "noImplicitAny": false,
+  }
 }
+
+
 
 module.exports = nextConfig
