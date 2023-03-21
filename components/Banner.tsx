534c1cd9c53580e8e49fdda704455bd0c7b62d6b
AryaK-31 <aryakaslikar31@gmail.com>
2023-03-21 11:42:08 +0530

    Rule Book Added

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import deved from '../public/sher.png'
 import Image from 'next/legacy/image'
 import Link from 'next/link';
+
 import { BsFillArrowRightCircleFill } from "react-icons/bs"
 import {
     AiFillInstagram,
-    AiFillCaretDown
+    AiFillCaretDown,
+    AiFillBook
 } from "react-icons/ai";
 
 export default function Banner() {
@@ -25,12 +27,36 @@ export default function Banner() {
                                     <span className='lg:text-blue-400 text-yellow-400 pl-4 pr-4 font-medium'>Let&#39;s get HUNT&#39;in</span>
                                     <span className='text-3xl md:pl-8 lg:pl-8 flex justify-center text-pink-600 cursor-pointer'>
                                         <Link href="https://anantya2023.pccoe.hosting.acm.org/about13" target={'_blank'} scroll={false}><BsFillArrowRightCircleFill /> </Link>
+                                                          
 
                                     </span>
+
                                 </p>
-                            </div>
-                        </div>
+                            </div>                            
+
+                        </div>                        
                     </div></Link>
+                    <Link href="https://docs.google.com/document/d/1WpqhCrIhSHs_nHF4OjU9m6TkoTZ7HbQmKZyODrbwiqM/edit?usp=sharing" target={'_blank'} scroll={false}><div className="relative group w-100 max-w-xl mt-12 mx-auto">
+                        <div className="absolute -inset-1 bg-gradient-to-r from-[#05c1fa] to-[#fa056b] rounded-lg blur opacity-95 group-hover:from-[#6f02e4] group-hover:to-[#fa0505] group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+                        <div className="relative py-1 bg-black ring-1 ring-gray-900/5 rounded-lg leading-none ">
+                            <div className="items-left">
+                                <p className="text-white px-3 py-2 flex justify-center text-xl lg:divide-x md:divide-x divide-gray-600">
+                                   
+                                    <span className='lg:text-blue-400 text-yellow-400 pl-4 pr-4 font-medium'>Rule Book</span>
+                                    <span className='text-3xl md:pl-8 lg:pl-8 flex justify-center text-pink-600 cursor-pointer'>
+                                        <Link href="https://docs.google.com/document/d/1WpqhCrIhSHs_nHF4OjU9m6TkoTZ7HbQmKZyODrbwiqM/edit?usp=sharing" target={'_blank'} scroll={false}><AiFillBook/> </Link>
+                                                          
+
+                                    </span>
+
+                                </p>
+                            </div>                            
+
+                        </div>                        
+                    </div></Link>                  
+                    
+                    
+                    
                 </div>
                 <div className='lg:hidden text-4xl py-1 gap-16 justify-center text-gray-600 flex'>
                     <Link href="#description" scroll={false}>
--- components/Description.tsx
+++ components/Description.tsx
@@ -63,7 +63,7 @@ export default function Description() {
                             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
                             <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
                                 <h1 className="font-dmserif text-3xl font-bold text-white mb-20">Prize Money</h1>
-                                <p className="mb-10 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Cash prize and goodies.</p>
+                                <p className="mb-10 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Cash prize of &#x20B9;3000 and goodies.</p>
                                 <div className=' w-full h-1/4'>
 
                                 </div>
